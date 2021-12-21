dad793f50212bd92fd0822314756157fcb662889
= <=>
2021-12-22 00:35:56 +0800

    a

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from itertools import product
 import json
 from os import lockf
 from flask import Flask,jsonify,request,Response
@@ -7,7 +8,7 @@ import hashlib
 import socket,sys,threading,unittest
 import json
 
-item = open(request.data)
+
 SERVER = "localhost:5000"
 JSON_CONTENT_TYPE = "application/json; charset=UTF-8"
 
@@ -18,12 +19,14 @@ HOST = "time-b-b.nist.gov"
 PORT = 13
 lock = threading.Lock()
 
-# def retrieve():
-#     global data1, data2, data3, data4
-#     with open(FILENAME) as file:
-#         data1, data2, data3, data4 = file.readline(), file.readline(), file.readline(), file.readline()
-#         data1, data2, data3, data4 = data1.split(), data2.split(), data3.split(), data4.split()
-#     data1, data3, data4 = [int(x) for x in data1], [int(x) for x in data3], [int(x) for x in data4]
+class itemx():
+    def get_one_item(id,desc,price,quantity):
+        return jsonify(
+                {"exe_id":obtain(),
+                "id":id,
+                "desc":desc,
+                "price":price,
+                "quantity":quantity},200)
 
 def obtain():
     global HOST, PORT
@@ -34,89 +37,84 @@ def obtain():
     exe_id = hashlib.sha256(time.encode("utf-8")).hexdigest()
     return exe_id
 
-# def get_daytime(host, port):
-#     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-#         s.connect((host, port))
-#         data = s.recv(1024)
-#     return data.decode('utf-8').strip()
 
+def readjson():
+    with open("product.json","r") as prodfile:
+        info = prodfile.dumps()
+    print(info)
 
-@app.route("/query/<int:id>")#,methods=["POST"]
+def checkId(id):
+
+    for i,item in enumerate(items["items"]):
+        if i["item"]["id"] == id:
+            
+            return True
+    return False
+
+def checkQty(quantity):
+    if i["item"]["quantity"] >= quantity:
+        return True
+    else: return False
+
+def checkCredit(creditCard):
+    if creditCard.length() >=16:
+        return True
+    else: return False
+
+@app.route("/api/query/<int:id>")#,methods=["POST"]
 def query(id):
-    data = json.loads(request.data)
+    data = json.loads("product.json")
     id = data.get("id")
-    for i,item in enumerate(id):
+    for item in data["items"]:
         if item["id"] == id:
-            return {"exe_id": exe_id, "success": True, "result": item}     
+            return jsonify({"exe_id": exe_id, "success": True, "result": item}),200     
     return jsonify({"exe_id": exe_id,"error": "todo item not found"}), 404
 
 
-@app.route("/buy/<int:id>/<int:quantity>/",method =["POST"])
-def buy(id,quantity):
+@app.route("/api/buy",methods =["GET"])
+def buy():
     global lock
-    # str(creditCardNo)
-    data = json.loads(request.data)
-    id = data.get("id")
-    quantityBuy = data.get("quantity")
-    for i,item in enumerate(items):
-        if items["id"] == id:
-            if (quantity<=item["quantity"]):
-                if (creditCardNo == creditC["id"] ):
-                    lock.acquire()
-                    finalprice = item["price"]*quantityBuy
-                    item["quantity"] -= quantityBuy
-                    lock.release()
-                    return {"exe_id": exe_id, "success": True, "result": item}
-                else:return jsonify({"exe_id": exe_id,"error": "Credit card invalid"}), 404
-            else: return jsonify({"exe_id": exe_id,"error": "Stock is not enough"}), 404
-        else: return jsonify({"exe_id": exe_id,"error": "todo item not found"}), 404
+    try:
+        data = json.loads(request.data)
+        if "id" in data:
+            id = data.get("id")
+        if "quantity" in data:
+            quantity = data.get("quantity")
+        if "creditCardNo" in data:
+            creditCardNo = data.get("creditCardNo")
+        
 
-@app.route("/replenish/<int:id>/<int:quantity>",method=["POST"])
-def replenish(id,quantity):
-    data = json.loads(request.data)
+        if (checkId(id)==True and checkQty(quantity)== True and checkCredit(creditCardNo)==True):
+            data = json.loads("product.json")
+            for items in data["items"]:
+                if items["id"] == id:
+                    items["quantity"] -= quantity
+                    json.dump("product.json",data,indent=4)
+                    return {"exe_id": exe_id, "success": True, "result": f"The total price: {items['price']*quantity}"}
+            
+        elif (checkId(id)==False): return jsonify({"exe_id": exe_id,"error": "Credit card invalid"}), 404
+        elif (checkQty(quantity)==False): return jsonify({"exe_id": exe_id,"error": "Stock is not enough"}), 404
+        elif (checkCredit(creditCardNo)==False): return jsonify({"exe_id": exe_id,"error": "todo item not found"}), 404
+    except:
+        
+
+@app.route("/api/replenish/",methods=["GET"])
+def replenish():
+    data = json.loads("product.json")
     id = data.get("id")
     quantityAdd = data.get("quantity")
+    creditCard = data.get()
     for i,item in enumerate(items):
         if items["id"] == id:
             items["quantity"] += quantityAdd
             return {"exe_id": exe_id, "success": True, "result": item}
         else: return jsonify({"exe_id":exe_id,"error": "todo item not found"}), 404
 
-@app.route("/api/query/<int:id>")#,methods=["POST"]
-def query(id):
-    data = json.loads(request.data)
-    for i,item in enumerate(items):
-        if items["id"] == id:
-            return {"exe_id": exe_id, "success": True, "result": item}     
-    return jsonify({"exe_id": exe_id,"error": "todo item not found"}), 404
-
-
-@app.route("/api/buy/<int:id>/<int:quantity>/<int:creditCardNo>",method =["POST"])
-def buy(id,quantity,creditCardNo):
-    data = json.loads(request.data)
-    for i,item in enumerate(items):
-        if items["id"] == id:
-            if (quantity<=item["quantity"]):
-                if (creditCardNo):
-                    finalprice = item["price"]*quantity
-                    item["quantity"] -= quantity
-                    return {"exe_id": exe_id, "success": True, "result": item}
-                else:return jsonify({"exe_id": exe_id,"error": "Credit card invalid"}), 404
-            else: return jsonify({"exe_id": exe_id,"error": "Stock is not enough"}), 404
-        else: return jsonify({"exe_id": exe_id,"error": "todo item not found"}), 404
-
-@app.route("/api/replenish/<int:id>/<int:quantity>",method=["POST"])
-def replenish(id,quantity):
-    data = json.loads(request.data)
-    for i,item in enumerate(items):
-        if items["id"] == id:
-            items["quantity"] += quantity
-            return {"exe_id": exe_id, "success": True, "result": item}
-        else: return jsonify({"exe_id":exe_id,"error": "todo item not found"}), 404
 
 if __name__ == "__main__":
     a_string = obtain()
     # exe_id = hashlib.sha256(a_string.encode("utf-8")).hexdigest()  
     print(obtain)
     app.run()
+    app.readjson()
     
